374514700d8d0204cb0c32244e67c0e44d92b9a6
SaltyChiang <SaltyChiang@users.noreply.github.com>
2025-12-16 12:15:42 -0500

    Quick patch.

--- generic_ks/fn_links_milc.c
+++ generic_ks/fn_links_milc.c
@@ -34,7 +34,10 @@ create_G_special(void){
     terminate(1);
   }
 
-  memset(m, '\0', sites_on_node*4*sizeof(su3_matrix));
+  size_t i;
+  FORALLFIELDSITES_OMP(i,){
+    memset(m+4*i, '\0', 4*sizeof(su3_matrix));
+  } END_LOOP_OMP
   return m;
 }
 
--- include/io_lat.h
+++ include/io_lat.h
@@ -44,6 +44,8 @@
 #define SAVE_PARALLEL_SCIDAC_DP          62
 #define SAVE_MULTIFILE_SCIDAC_DP         63
 #define SAVE_PARTFILE_SCIDAC_DP          64
+#define RELOAD_MPIIO                     65
+#define SAVE_MPIIO                       66
 
 /* Format for NERSC archive files */
 #define ARCHIVE_3x2   0
@@ -332,6 +334,8 @@ gauge_file *restore_serial(const char *filename);
 gauge_file *save_serial(const char *filename);
 gauge_file *restore_parallel(const char *filename);
 gauge_file *save_parallel(const char *filename);
+gauge_file *restore_mpiio(const char *filename);
+gauge_file *save_mpiio(const char *filename);
 gauge_file *save_checkpoint(const char *filename);
 gauge_file *save_serial_archive(const char *filename);
 gauge_file *save_parallel_archive(const char *filename);
